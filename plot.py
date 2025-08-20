c46772b562ab5f65a51ed51417ddaf08b410fb8d
Machative <machjayus@gmail.com>
2025-08-20 11:30:01 -0400

    Plot panel functional

--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 import numpy as np
+import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
-    QLabel, QDateEdit, QDialog, QToolBar, QComboBox, QStackedWidget, 
+    QLabel, QDateTimeEdit, QDialog, QToolBar, QComboBox, QStackedWidget, 
     QTabWidget, QPushButton, QTextEdit
 )
 from PySide6.QtCore import QDate, Qt
@@ -30,14 +32,14 @@ class PlotPanel(QWidget):
         # Row 2: Date selectors
         date_layout = QHBoxLayout()
         label1 = QLabel("Start Date:")
-        self.date1 = QDateEdit()
+        self.date1 = QDateTimeEdit()
         self.date1.setCalendarPopup(True)
-        self.date1.setDate(QDate.currentDate())
+        self.date1.dateTimeChanged.connect(self.updatePlot)
 
         label2 = QLabel("End Date:")
-        self.date2 = QDateEdit()
+        self.date2 = QDateTimeEdit()
         self.date2.setCalendarPopup(True)
-        self.date2.setDate(QDate.currentDate())
+        self.date2.dateTimeChanged.connect(self.updatePlot)
 
         date_layout.addWidget(label1)
         date_layout.addWidget(self.date1)
@@ -46,22 +48,59 @@ class PlotPanel(QWidget):
         date_layout.addWidget(self.date2)
 
         # Graph
-        x_data = np.linspace(0, 10, 100)
-        y_data = np.sin(x_data)
+        self.x_data = []
+        self.y_data = []
 
-        fig, ax = plt.subplots()
-        ax.plot(x_data, y_data, label="Sample Data")
-        ax.set_xlabel("X Axis")
-        ax.set_ylabel("Y Axis")
-        ax.legend()
+        fig, self.ax = plt.subplots()
+        (self.line,) = self.ax.plot([],[])
+        self.ax.set_xlabel("Time")
+        self.ax.set_ylabel("Value")
 
         self.canvas = FigureCanvas(fig)
+
+        self.date1.setDate(QDate.currentDate())
+        self.date2.setDate(QDate.currentDate())
 
         # Assemble Layout
         layout.addLayout(top_row)
         layout.addLayout(date_layout)
         layout.addWidget(self.canvas)
         self.setLayout(layout)
+
+    def readSelectedData(self):
+        self.x_data.clear()
+        self.y_data.clear()
+        
+        fromDate = self.date1.dateTime().toString("yyyy-MM-dd_hh:mm")
+        toDate = self.date2.dateTime().toString("yyyy-MM-dd_hh:mm")
+        if self.currentDev:
+            devData = "res/"+self.currentDev.getUUID()+"_log.csv"
+            with open(devData) as fp:
+                reader = csv.reader(fp, delimiter=",",quotechar='"')
+                for row in reader:
+                    timestamp, frac = row[0].split(".")
+                    if timestamp >= fromDate and timestamp <= toDate:
+                        dt = datetime.strptime(timestamp,"%Y-%m-%d_%H:%M:%S")
+                        us = int(frac)*10_000
+                        dt = dt.replace(microsecond=us)
+                        epochTime = dt.timestamp()
+                        self.x_data.append(epochTime)
+                        self.y_data.append(float(row[1]))
+
+
+    def updatePlot(self):
+        self.readSelectedData()
+
+        # Update the line data
+        self.line.set_xdata(self.x_data)
+        self.line.set_ydata(self.y_data)
+
+        # Adjust axis limits
+        self.ax.relim()
+        self.ax.autoscale_view()
+
+        # Refresh the canvas
+        self.canvas.draw_idle()
 
     def deviceSelected(self, devices):
         for dev in devices:
